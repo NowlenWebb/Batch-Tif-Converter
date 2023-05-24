9d74d056d68deb3d91c4723a7c1e961677a908e9
Joshua Nowlen Webb <32349879+NowlenWebb@users.noreply.github.com>
2023-05-23 18:04:59 -0700

    Create tif_converter.ps1

---
+++ tif_converter.ps1
@@ -0,0 +1,27 @@
+#Path to Ghostscript EXE
+$tool = '<Path to GhostScript EXE>'
+
+#Directory containing PDFs to be converted
+$inputDir = '<Input Directory>'
+
+#Directory where TIFs will be saved
+$outputDir = '<Output Directory>'
+
+$pdfs = get-childitem $inputDir | where {$_.Extension -match "pdf"}
+
+foreach($pdf in $pdfs)
+{
+    $tif = $outputDir + $pdf.BaseName + ".tif"
+    if(test-path $tif)
+    {
+        "tif file already exists " + $tif
+    }
+    else        
+    {   
+        "Processing " + $pdf.Name        
+        $param = "-sOutputFile=$tif"
+        & $tool -q -dNOPAUSE -sDEVICE=tiffg4 $param -r300 $pdf.FullName -c quit
+    }
+}
+
+EXIT
